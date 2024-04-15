e22b975985c54828c3828a29b15d962da3a06dfd
tatendabakozw <tatendabakozw@gmail.com>
2024-04-15 23:09:57 +0200

    feature added hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/page-sections/HeroSection";
 import GeneralLayout from "@/layouts/GeneralLayout";
 
 export default function Home() {
   return (
     <GeneralLayout>
-      <div className="herosection min-h-[70vh] bg-red-100 w-full">sadfs</div>
+      <HeroSection />
     </GeneralLayout>
   );
 }
